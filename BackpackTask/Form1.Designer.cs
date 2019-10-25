0ea40ffcf4a0628814ac8a83ec60e9b4a3d92948
nickiteks <splint38@gmail.com>
2019-10-25 17:30:41 +0300

    finish

--- BackpackTask/Form1.Designer.cs
+++ BackpackTask/Form1.Designer.cs
@@ -168,7 +168,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(724, 303);
+            this.ClientSize = new System.Drawing.Size(745, 312);
             this.Controls.Add(this.textBoxStart);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
